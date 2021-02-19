527d073d6663ba74dd6b632b7bf7f6bf9317373d
Julian <julian@ashleywreford.com>
2021-02-19 15:03:06 +0000

    Basic opening of file success

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,43 @@
 #include <jni.h>
 #include <string>
+#include <UbiForm/Component.h>
+#include <fstream>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_ubiformandroidstreamingexample_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+Component * component;
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_ubiformandroidstreamingexample_MainActivity_startComponent(JNIEnv *env,
+                                                                            jobject thiz,
+                                                                            jstring ip_address) {
+    try {
+        if (component == nullptr) {
+            jboolean isCopy = false;
+            std::string componentUrl = env->GetStringUTFChars(ip_address, &isCopy);
+            component = new Component(componentUrl);
+        }
+        if (component->getBackgroundPort() == -1) {
+            component->startBackgroundListen();
+        }
+    } catch (std::logic_error &e) {
+        std::string returnString = "Error with component startup: " + std::string(e.what());
+    }
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_ubiformandroidstreamingexample_MainActivity_openFile(JNIEnv *env, jobject thiz,
+                                                                      jstring file_loc) {
+    jboolean isCopy = false;
+    std::string fileLoc = env->GetStringUTFChars(file_loc, &isCopy);
+    std::ifstream file;
+    file.open(fileLoc);
+    if (file.is_open()) {
+        char bytes[10];
+        file.read(bytes, 10);
+        bytes[9] = 0;
+        return env->NewStringUTF(bytes);
+    }else{
+        std::string ret = "Error " + fileLoc;
+        return env->NewStringUTF(ret.c_str());
+    }
 }
--- app/src/main/java/com/example/ubiformandroidstreamingexample/MainActivity.kt
+++ app/src/main/java/com/example/ubiformandroidstreamingexample/MainActivity.kt
@@ -1,23 +1,54 @@
 package com.example.ubiformandroidstreamingexample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Parcel
+import android.os.ParcelFileDescriptor
+import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
+    val PICKFILE_RESULT_CODE = 1
+    private var fileUri: Uri? = null
+    private var filePath: String? = null
+    private var mParcelFileDescriptor : ParcelFileDescriptor? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Example of a call to a native method
-        findViewById<TextView>(R.id.sample_text).text = stringFromJNI()
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
+    fun fileButtonClick(view: View){
+        var chooseFile = Intent(Intent.ACTION_GET_CONTENT)
+        chooseFile.type = "*/*"
+        chooseFile = Intent.createChooser(chooseFile, "Choose a file")
+        startActivityForResult(chooseFile, PICKFILE_RESULT_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            PICKFILE_RESULT_CODE -> {
+                val text = findViewById<TextView>(R.id.file_path_text)
+                if (resultCode == -1) {
+                    fileUri = data?.data
+
+                    mParcelFileDescriptor =
+                        contentResolver.openFileDescriptor(fileUri!!, "r")
+
+                    val fd: Int? = mParcelFileDescriptor?.getFd()
+                    text.text = openFile("/proc/self/fd/$fd")
+                }
+            }
+        }
+    }
+
+
+    external fun startComponent(ipAddress: String)
+    external fun openFile(fileLoc: String) : String
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
